de47c35120f183957e89ee5f213c6e6a42e96d9f
Muchiri-cmd <davismuchiri21@gmail.com>
2024-05-17 12:58:22 +0300

    Add starter assets

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert("Working perfectly fine.")
+import './styles.css';
+import coverImg from './kfc.jpeg';
+import _ from 'lodash';
+
+function component(){
+    const contentDiv = document.getElementById('content')
+    contentDiv.innerHTML = _.join(['Finger','lickin','good'],' ');
+    contentDiv.classList.add('content');
+    
+    //add background image
+    const bgImage = new Image();
+    bgImage.src = coverImg;
+    contentDiv.appendChild(bgImage);
+    return contentDiv;
+}
+component();
+console.log("Working perfectly fine.")
